ba7b6a340650dc0fd65c69062f650b17172d6b23
akberofh <sd85bejal@code.edu.az>
2024-10-20 00:35:28 +0400

    deploycors

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const server = http.createServer(app);
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: ['https://grez-frontend.vercel.app', 'http://localhost:3000'],
     credentials: true,
 }));
 app.use(express.urlencoded({ extended: true }));
